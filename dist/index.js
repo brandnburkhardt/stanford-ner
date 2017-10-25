ba51b9bc3e334fbc04086ae886227ce44745ce78
Brandon Burkhardt <brandon.burkhardt@crosschx.com>
2017-10-25 16:09:21 -0400

    new dist files from typescript compile

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var NER_1 = require("./src/NER");
+exports.NER = NER_1.NER;
+var FileNotFoundError_1 = require("./src/FileNotFoundError");
+exports.FileNotFoundError = FileNotFoundError_1.FileNotFoundError;
+//# sourceMappingURL=index.js.map
---
+++ dist/src/FileNotFoundError.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Thrown when a file cannot be found
+ */
+class FileNotFoundError extends Error {
+}
+exports.FileNotFoundError = FileNotFoundError;
+//# sourceMappingURL=FileNotFoundError.js.map
